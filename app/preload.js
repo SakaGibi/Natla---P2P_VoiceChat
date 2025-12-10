615b83aee3f8b3a9935690888c3f3dd32836a51f
Emre <95179013+SakaGibi@users.noreply.github.com>
2025-12-10 11:23:50 +0300

    ses seviyesine bakmak için metering eklendi.

--- app/preload.js
+++ app/preload.js
@@ -1,5 +1,9 @@
 const { contextBridge } = require('electron');
 
+let audioContext;
+let analyser;
+let source;
+
 contextBridge.exposeInMainWorld('electronAPI', {
     getUserName: async () => {
         return prompt("Lütfen adını gir:");
@@ -13,13 +17,25 @@ contextBridge.exposeInMainWorld('audioAPI', {
             const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
             console.log("🎤 Preload: Mikrofon stream hazır");
 
-            const audioContext = new AudioContext();
-            const source = audioContext.createMediaStreamSource(stream);
-            source.connect(audioContext.destination);
+            audioContext = new AudioContext();
+            source = audioContext.createMediaStreamSource(stream);
+
+            analyser = audioContext.createAnalyser();
+            analyser.fftSize = 256;
+
+            source.connect(analyser);
+            analyser.connect(audioContext.destination);
 
             console.log("🟢 Preload: Mikrofon sesi hoparlöre yönlendirildi.");
         } catch (err) {
             console.error("❌ Preload: Mikrofon alınamadı:", err);
         }
+    },
+
+    getAudioData: () => {
+        if (!analyser) return null;
+        const dataArray = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteTimeDomainData(dataArray);
+        return dataArray;
     }
 });
